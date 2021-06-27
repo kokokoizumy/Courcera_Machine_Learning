185eaabaa16b31cc8cc17ba0ea84ed4d58a7d658
kokokoizumy <kokokoizumy@users.noreply.github.com>
2021-06-28 00:00:18 +0900

    practice3 made

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -17,7 +18,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
+    hx = X*theta;
+    theta = theta - alpha*(1/m)*(sum((hx - y).*X))';
 
 
 
