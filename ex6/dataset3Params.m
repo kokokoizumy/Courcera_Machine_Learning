bbed850fdbc375d99c71e8164804878368b991fc
koizumi yukihiro <78544232+kokokoizumy@users.noreply.github.com>
2021-07-06 17:45:28 +0900

    SVM Clear

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,41 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%svmPredictを使うための準備
+x1 = [1 2 1]; x2 = [0 4 -1];
 
 
+%探索範囲の準備
+C_Vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_Vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+num_c = length(C_Vec);
+num_s = length(sigma_Vec);
 
+result = zeros(num_c,num_s);
 
+%for loop二つで探索を行う
+for ii = 1:num_c
+C = C_Vec(ii);
 
+for jj = 1:num_s
+sigma = sigma_Vec(jj);
+
+%modelの定義これでいいのか悪いのか？
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+
+%関数simPredictで値の推測をしてもらう
+predictions = svmPredict(model,Xval);
+
+%結果を得る
+result(ii,jj) =  mean(double(predictions ~= yval));
+end
+end
+
+[val,id]=min(result(:));
+[ii,jj] = ind2sub(size(result),id);
+
+C = C_Vec(ii);
+sigma = sigma_Vec(jj);
 
 % =========================================================================
 
